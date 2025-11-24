a819deab7313f67728041e308002a2d92279e7da
salih damar <bilprogeem@gmail.com>
2025-11-25 02:52:55 +0300

    Configure admin panel for categories and products

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock', 'is_home','is_active', 'category', 'created_at', 'updated_at')
+    list_filter = ('is_home','is_active', 'category')
+    search_fields = ('name', 'description')
+    prepopulated_fields = {'slug': ('name',)}
+    ordering = ('name',)
