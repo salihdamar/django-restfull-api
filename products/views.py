a7b4d980cd7f313d7e716b983d4701373a7506f7
salih damar <bilprogeem@gmail.com>
2025-11-26 01:40:49 +0300

    JSON Response

--- products/views.py
+++ products/views.py
@@ -1,3 +1,31 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def product_list(request):
+    products= Product.objects.all()
+    data= {'products': list(products.values())}
+    return JsonResponse(data)
+
+
+def product_details(resquest, pk):
+    try:
+        product= Product.objects.get(pk=pk)
+        data={
+            'id': product.id,
+            'name': product.name,
+            'slug': product.slug,
+            #'category': product.category.id, #hata verdi düzenle burayı
+            'stock': product.stock,
+            'description': product.description,
+            'price': str(product.price),
+            'is_home': product.is_home,
+            'is_active': product.is_active,
+
+        }
+        return JsonResponse(data)
+    
+    except Product.DoesNotExist:
+        return JsonResponse({'eror':'Productnot found'},status=404)
+       
